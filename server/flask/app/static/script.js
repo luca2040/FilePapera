45ebeb18cf01d79506489db868898a6cd18a5434
Luca2040 <apollo.2040.24@gmail.com>
2024-09-14 13:45:23 +0200

    added base script

---
+++ server/flask/app/static/script.js
@@ -0,0 +1,16 @@
+async function fetchDataAndParseJSON() {
+  const url = "/list";
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    const data = await response.json();
+    console.log("Data received:", data);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+window.onload = fetchDataAndParseJSON;
