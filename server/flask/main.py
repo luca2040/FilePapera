801993b1a768c9b60ae11bc73cc0917ec9eeed8a
Luca2040 <152313871+Luca2040@users.noreply.github.com>
2025-05-06 18:37:33 +0200

    added option for developing in container

--- server/flask/main.py
+++ server/flask/main.py
@@ -1,10 +1,4 @@
-import os
-from dotenv import load_dotenv
-
-if not os.getenv("FLASK_ENV"):  # for development
-    load_dotenv()
-
 from app.Server import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
