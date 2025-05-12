47c7fdde1da09b8bd1cbf6ae7d6363d2230038d7
Luca2040 <152313871+Luca2040@users.noreply.github.com>
2025-05-12 18:36:45 +0200

    added extra files for debug

--- server/flask/main.py
+++ server/flask/main.py
@@ -6,4 +6,20 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        extra_files=[
+            "/app/app/static/scripts/index/ui/functions.js",
+            "/app/app/static/scripts/index/ui/ui.js",
+            "/app/app/static/scripts/index/utils/api.js"
+            "/app/app/static/scripts/index/utils/constants.js",
+            "/app/app/static/scripts/index/utils/page.js",
+            "/app/app/static/scripts/index/fileview.js",
+            "/app/app/static/scripts/index/script.js",
+            "/app/app/static/scripts/index/upload.js",
+            "/app/app/static/scripts/login/script.js",
+            "/app/app/static/styles/style.css",
+        ],
+    )
